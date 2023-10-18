dde3deceab3652dd60c60987019bcbfec6f4e746
Jonathan Allen <jallen@snow.edu>
2023-10-18 10:59:13 -0600

    Add project files.

---
+++ BlazorApp1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+}
